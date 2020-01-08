778b5af749eb0263b1f746da09d189cebe5e5a67
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-01-08 15:44:40 +0900

    Update html

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ const page = `<!DOCTYPE html>
 				text-align: center;
 				display: block;
             };
-        </style>
+		</style>
+		<title>MISWアクセスカウンター</title>
     </head>
 	<body>
 		<div class="title">
-			<h1 class="title">modokiアクセスカウンター</h1>
+			<h1 class="title">MISWアクセスカウンター</h1>
 			<p>あなたは%d番目の来訪者です！</p>
 			<a href="https://twitter.com/share" class="twitter-share-button" data-url="https://counter.apps.tsuzu.xyz/" data-text="あなたは%d番目の来訪者です！"data-size="default" data-count="none">Tweet</a> 
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
